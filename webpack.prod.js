ef4b106f4b6f0557314ce0cba8ec10d9cae6c665
Pratik Agarwal <pratik1234agarwal@gamil.com>
2020-07-11 13:28:21 +0530

    Fixed typos

--- webpack.prod.js
+++ webpack.prod.js
@@ -7,7 +7,7 @@ const TerserPlugin = require('terser-webpack-plugin');
 const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
-    entry: './src/client/index.js',
+    entry: './src/Client/index.js',
     mode: 'production',
     output: {
         libraryTarget: 'var',

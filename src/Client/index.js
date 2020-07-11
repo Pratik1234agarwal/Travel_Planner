f6aeab2efdd687f6aab888a267190938a155fffa
Pratik Agarwal <pratik1234agarwal@gamil.com>
2020-07-11 15:45:12 +0530

    Added Materialize.css to the project

---
+++ src/Client/index.js
@@ -0,0 +1,3 @@
+import 'materialize-css/dist/css/materialize.css';
+import 'materialize-css/dist/js/materialize.min';
+
--- webpack.dev.js
+++ webpack.dev.js
@@ -23,6 +23,10 @@ module.exports = {
             {
                 test: /\.scss$/,
                 use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+            },
+            {
+                test: /\.css$/,
+                use:['style-loader','css-loader']
             }
         ]
     },

005edeefaa8481f2d7afc10d0d90ddd9498b9ff8
Pratik Agarwal <pratik1234agarwal@gamil.com>
2020-07-15 12:26:24 +0530

    Added Api-integration and a modal to show the results

--- src/Client/index.js
+++ src/Client/index.js
@@ -15,11 +15,70 @@ M.Slider.init(sidenav,{
     interval:5000
 });
 
+
+// Modal 
+
+const modal = document.querySelectorAll('.modal');
+M.Modal.init(modal,{
+    dismissible:false,
+    onOpenEnd : carouselInit
+});
+
 // Datepicker 
 
 const picker = document.querySelector('.datepicker');
 M.Datepicker.init(picker,{
     autoclose:true,
     showClearBtn:true,
-    minDate:new Date()
+    minDate:new Date(),
 });
+
+
+function carouselInit(){
+    const carousel = document.querySelector('.carousel');
+    M.Carousel.init(carousel,{
+        fullWidth:true,
+        indicators:true,
+    });
+}
+
+
+const button = document.querySelector(".btn");
+button.addEventListener('click',async()=>{
+    const input = document.querySelector('input');
+    const data = {city:input.value};
+    const apiData = await postData("http://localhost:8000/fetch",data);
+    console.log(apiData);
+    setImages(apiData.images);
+    const text = document.querySelector("#Image-text");
+    text.innerHTML = `${apiData.cityName},${apiData.countryName}`;
+    
+});
+
+function setImages(images){
+    for(let i=0;i<4;i++){
+        const carousel = document.querySelectorAll('.carousel-item');
+        carousel[i].querySelector('img').src = images[i];
+    }
+}
+
+
+const postData = async(url="",data={})=>{
+    const response = await fetch(url,{
+        method:'POST',
+        credentials:'same-origin',
+        headers:{
+            'Content-Type':'application/json',
+        },
+        body:JSON.stringify(data),
+    });
+    console.log(response);
+
+    try{
+        const data = await response.json();
+        return data;
+    }catch(error){
+         console.log("Error",error);
+         return "error";
+    }
+}
